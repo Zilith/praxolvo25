e7a7a65fdc4b873f87c4063850452c41e3860639
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-05-10 20:28:23 -0500

    feat(models/product): add validators for the data
    add validators to ensuere the type of the data and the existent of it

--- ap/models/product.js
+++ ap/models/product.js
@@ -1,6 +1,17 @@
 class Product {
     constructor(id, name, descr, price, creationDate) {
         this.id = id;
+        // data validation for missing or typeof
+        if (!name || typeof name !== 'string') {
+            throw new Error('Invalid or missing name');
+        }
+        if (!descr || typeof descr !== 'string') {
+            throw new Error('Invalid or missing description');
+        }
+        if (!price || typeof price !== 'number') {
+            throw new Error('Invalid or missing price');
+        }
+
         this.name = name;
         this.descr = descr;
         this.price = price;
