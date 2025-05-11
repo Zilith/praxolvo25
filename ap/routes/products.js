0424657c6abe63249901fe8210d5751e1da3a6e1
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-05-11 00:31:27 -0500

    feat: add try catch to handle all errors in errorHandler middeware
    use next(error) to handle all the general errors

--- ap/routes/products.js
+++ ap/routes/products.js
@@ -21,12 +21,16 @@ let products = [
 ];
 
 // GET /products - Get all products
-router.get('/', (req, res) => {
-    res.status(200).json(products);
+router.get('/', (req, res, next) => {
+    try {
+        res.status(200).json(products);
+    } catch (error) {
+        next(error);
+    }
 });
 
 // POST /products - Create product
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     const { name, descr, price } = req.body;
     try {
         const newProduct = new Product({
@@ -36,46 +40,52 @@ router.post('/', (req, res) => {
         });
         products.push(newProduct);
         res.status(201).json(newProduct);
-    } catch (err) {
-        res.status(400).send({ error: err.message });
+    } catch (error) {
+        next(error);
     }
 });
 
 /**
  * Were missing some routes here...
  */
-router.get('/:id', (req, res) => {
-    const product = products.find((p) => p.id === req.params.id);
-    console.log(product);
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).end();
+router.get('/:id', (req, res, next) => {
+    try {
+        const product = products.find((p) => p.id === req.params.id);
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).end();
+        }
+    } catch (error) {
+        next(error);
     }
 });
 
-router.put('/:id', (req, res) => {
-    const product = products.find((p) => p.id === req.params.id);
-
-    if (!product) {
-        return res.status(404).json({ error: 'Product not found ' });
-    }
+router.put('/:id', (req, res, next) => {
     try {
+        const product = products.find((p) => p.id === req.params.id);
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
         product.update(req.body);
         res.json(product);
-    } catch (err) {
-        res.status(400).send({ error: err.message });
+    } catch (error) {
+        next(error);
     }
 });
 
-router.delete('/:id', (req, res) => {
-    const initialLength = products.length;
-    products = products.filter((p) => p.id !== req.params.id);
+router.delete('/:id', (req, res, next) => {
+    try {
+        const initialLength = products.length;
+        products = products.filter((p) => p.id !== req.params.id);
 
-    if (products.length === initialLength) {
-        return res.status(404).send({ error: 'Product not found' });
+        if (products.length === initialLength) {
+            return res.status(404).send({ error: 'Product not found' });
+        }
+        res.status(204).end();
+    } catch (error) {
+        next(error);
     }
-    res.status(204).end();
 });
 
 module.exports = router;
--- ap/utils/middleware.js
+++ ap/utils/middleware.js
@@ -3,8 +3,12 @@ const unknownEndpoint = (req, res) => {
 };
 
 const errorHandler = (error, req, res, next) => {
+    // specific error handling
     if (error.name === 'SyntaxError') {
         res.status(400).send({ error: error.message });
+    } else {
+        // general error handling
+        res.status(400).send({ error: error.name, message: error.message });
     }
 };
 
